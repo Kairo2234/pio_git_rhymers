f3922fda6593e5c16de1957b9a094cdf2a7399c1
Kacper <251138@edu.p.lodz.pl>
2025-03-19 21:28:24 +0100

    fields-naming

--- src/main/java/edu/kis/vh/nursery/defaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/defaultCountingOutRhymer.java
@@ -1,7 +1,7 @@
 package edu.kis.vh.nursery;
 
-public class defaultCountingOutRhymer {
-
+public class DefaultCountingOutRhymer {
+    
     private int[] NUMBERS = new int[12];
 
     public int total = -1;
